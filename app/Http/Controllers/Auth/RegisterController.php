9264463c1a909e570d338a4d5a94d64e25f7882f
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-07-27 09:52:06 +0200

    Sobrescrevendo as regras de validação de senha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:4', 'confirmed'],
+            'password' => ['required', 'string', 'min:5', 'confirmed'],
             // 'confirmed faz a validação entre o nome do campo mais _confirmation
         ]);
     }
