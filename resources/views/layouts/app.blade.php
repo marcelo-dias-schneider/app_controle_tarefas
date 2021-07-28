06c4cf6679d2e9204f4a9819245e42aa5417e099
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-07-29 00:48:24 +0200

    Atualizando o menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('tarefa.index') }}">
+                                        {{ 'Ver tarefas' }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('tarefa.create') }}">
+                                        {{ 'Criar tarefas' }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
