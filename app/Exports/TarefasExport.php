dbfa89e1044d672c77de530a255e516c1c4a2732
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-08-01 17:09:05 +0200

    implementado os titutlos nas exportações

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -3,9 +3,11 @@
 namespace App\Exports;
 
 use App\Models\Tarefa;
+use Illuminate\Support\Arr;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +15,17 @@ class TarefasExport implements FromCollection
     public function collection()
     {
         return auth()->user()->tarefas()->get();
+    }
+
+    public function headings():array
+    {
+        return [
+            'ID tarefa',
+            'ID usuario',
+            'Tarefa',
+            'Data conclusão',
+            'Data criação',
+            'Data edição'
+        ];
     }
 }
